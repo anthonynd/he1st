a9b7225df75a91a64d6b5a2d01ed6eebb754a4d0
Asim Shrestha <asims178@gmail.com>
2019-04-27 09:14:06 -0400

    Adding better keys for movement Better file structure

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-export var speed = 150
+export var speed = 400
 var screen_size
 
 # Called when the node enters the scene tree for the first time.
@@ -9,13 +9,13 @@ func _ready():
 	
 func _process(delta):
 	var velocity = Vector2()
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
+	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
