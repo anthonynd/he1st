8f08cbdfc016b22f37d01ff4678b2ee464a82280
Asim Shrestha <asims178@gmail.com>
2019-04-28 19:47:03 -0400

    Loading Map and adding Main Menu

--- scripts/globals.gd
+++ scripts/globals.gd
@@ -1,13 +1,19 @@
 extends Node
 
+var debugMessages = [];
+
 var currentScene = null
+var mapNode = null
+var currentMap = null
 var gui = null
 
-var debugMessages = [];
+var player = null;
 
 func _ready():
 	currentScene = get_tree().root.get_child( get_tree().root.get_child_count() - 1)
 	gui = currentScene.get_node("gui/main_gui")
+	mapNode = currentScene.get_node("map")
+	player = currentScene.get_node("player")
 	pass # Replace with function body.
 
 func printOnScreen(value):
@@ -18,3 +24,16 @@ func printOnScreen(value):
 			debugMessages.remove(0)
 		debug_label.text = PoolStringArray(debugMessages).join("\n");
 	pass
+	
+func setMap(mapName):
+	if(!mapNode):
+		return
+	
+	if(currentMap):
+		currentMap.queue_free()
+	var s = ResourceLoader.load("res://level/maps/"+mapName+".tscn")
+	currentMap = s.instance()
+	mapNode.add_child(currentMap)
+	if(!player.visible):
+		player.visible = true
+	player.translate(currentMap.get_node("playerSpawn").transform.origin)
---
+++ scripts/gui/mainMenu/mainMenu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+func _ready():
+	var startBtn = $HBoxContainer/mainMenuButtons/startGameBtn
+	var exitBtn = $HBoxContainer/mainMenuButtons/exitGameBtn
+	startBtn.connect("pressed", self, "_set_start_map")
+	exitBtn.connect("pressed", self, "_exit_game")
+	pass # Replace with function body.
+
+func _set_start_map():
+	visible = false
+	get_node("/root/globals").setMap("house")
+	
+func _exit_game():
+	get_tree().quit()
