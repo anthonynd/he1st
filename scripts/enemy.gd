988d1d1c50c4706a3e32dbd35e5d882ef485408a
Anthony Nguyen-Duong <aat2nguy@uwaterloo.ca>
2019-04-29 11:59:31 -0400

    cleanup

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,9 +5,6 @@ onready var parent = get_parent()
 export (int) var detect_radius
 export (int) var FOV
 
-var angle = 0
-var direction = Vector2()
-
 const RED = Color(1.0, 0, 0, 0.4)
 const YELLOW = Color(1.0, 1.0, 0, 0.4)
 var fov_color = YELLOW
@@ -31,7 +28,11 @@ func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
 	for i in range(nb_points+1):
     	var angle_point = angle_from + i*(angle_to-angle_from)/nb_points
     	points_arc.push_back(center + Vector2( cos( deg2rad(angle_point) ), sin( deg2rad(angle_point) ) ) * radius)
+	
+	# Set detection area to the created polygon
 	$DetectionArea/CollisionPolygon2D.polygon = points_arc
+	
+	# Draw the polygon
 	draw_polygon(points_arc, colors)
 
 func _on_DetectionArea_body_entered(body):
@@ -39,7 +40,6 @@ func _on_DetectionArea_body_entered(body):
 		fov_color = RED
 		update()
 	pass # Replace with function body.
-
 
 func _on_DetectionArea_body_exited(body):
 	if body.name == "player":
