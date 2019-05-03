a0c491a26083c25d1bddd6b194bacf1049efc0a5
Anthony Nguyen-Duong <aat2nguy@uwaterloo.ca>
2019-05-03 11:59:07 -0400

    set camera/player limits

--- levels/house.gd
+++ levels/house.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
-func _on_shoot(bullet, _position, _direction):
-	var b = bullet.instance()
-	add_child(b)
-	b.start(_position, _direction)
-
+func _ready():
+	set_camera_limits()
+	
+func set_camera_limits():
+	var map_limits = $middleLayer.get_used_rect()
+	var map_cellsize = $middleLayer.cell_size
+	$player/mainCam.limit_left = map_limits.position.x * map_cellsize.x
+	$player/mainCam.limit_right = map_limits.end.x * map_cellsize.x
+	$player/mainCam.limit_top = map_limits.position.y * map_cellsize.y
+	$player/mainCam.limit_bottom = map_limits.end.y * map_cellsize.y
