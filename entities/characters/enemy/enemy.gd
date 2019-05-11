88712d83973e015c513fa5437c12fd7554aebf8b
Asim Shrestha <asims178@gmail.com>
2019-05-11 19:59:42 -0400

    Adding timer to enemy when it detects/get's shot

--- entities/characters/enemy/enemy.gd
+++ entities/characters/enemy/enemy.gd
@@ -7,44 +7,49 @@ onready var parent = get_parent()
 export (int) var detect_radius
 export (int) var FOV
 
+export (float) var totalWaitTimeInSeconds = 2.0 # Second
+
 const RED = Color(1.0, 0, 0, 0.5)
 const YELLOW = Color(1.0, 1.0, 0, 0.2)
 const WHITE = Color(1, 1, 1, 0.2)
-var fov_color = WHITE
 
+var fov_color = WHITE
 var target
 var hit_pos
 
-enum State {
-	IDLE,
-	PATROL,
-	DETECT,
-	ATTACK,
-	DEAD
-}
+enum State {IDLE, PATROL, DETECT, ATTACK, SHOT, DEAD}
 
 var currentState = State.IDLE
 var previousState = null
 
+var timer
+
 func _ready():
 	add_to_group("enemies")
 	set_slot(0)
+	
+	timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = totalWaitTimeInSeconds
+	timer.one_shot = true
+	timer.connect("timeout", self, "_on_timer_timeout")
 
 func _physics_process(delta):
-	$debug.text = "%s" % State.keys()[currentState]
+	$debug.text = "%s\n%s" % [State.keys()[currentState], timer.time_left]
 	
-	if (not is_dead() 
-		and currentState != State.DETECT
-		and parent is PathFollow2D):
-		setState(State.PATROL)
-		rotation = 0
-		parent.set_offset(parent.get_offset() + speed * delta)
-		parent.rotate = true
-	
-	if target:
-		aim()
-	else:
-		setState(State.PATROL)
+	if currentState != State.DEAD:
+		if timer.time_left == 0:
+			if (currentState != State.DETECT and parent is PathFollow2D):
+				setState(State.PATROL)
+				rotation = 0
+				parent.set_offset(parent.get_offset() + speed * delta)
+				parent.rotate = true
+		else:
+			# TODO: LOOK AROUND
+			pass
+			
+		if target:
+			aim()
 
 func aim():
 	var space_state = get_world_2d().direct_space_state
@@ -53,9 +58,9 @@ func aim():
 		hit_pos = result.position
 		update()
 		if result.collider.name == "player":
-			setState(State.DETECT) 
-			
+			setState(State.DETECT)
 			look_at(hit_pos)
+			timer.start()
 			
 			if slots[current_slot]:
 				slots[current_slot].shoot()
@@ -68,7 +73,7 @@ func aim():
 func _draw():
 	draw_circle_arc_poly(Vector2(), detect_radius, -FOV/2, FOV/2, fov_color)
 	
-	if target:
+	if target and hit_pos:
 		draw_circle(to_local(hit_pos), 5, RED)
 		draw_line(Vector2(), to_local(hit_pos), RED)
 
@@ -93,6 +98,12 @@ func setState(state: int):
 		previousState = currentState
 		currentState = state
 
+func damage(health_point):
+	setState(State.SHOT)
+	if timer.time_left == 0:
+		timer.start()
+	return .damage(health_point)
+
 func die():
 	.die()
 	$Body.modulate = Color(0.7, 0.7, 0.7, 0.4)
@@ -100,6 +111,10 @@ func die():
 	$DetectionArea/CollisionPolygon2D.disabled = true
 	target = null
 	setState(State.DEAD)
+
+func _on_timer_timeout():
+	if(currentState != State.DEAD):
+		setState(previousState)
 
 func _on_DetectionArea_body_entered(body):
 	if not target:
