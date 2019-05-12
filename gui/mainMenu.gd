30866065938de8ef58fc71004b3ddd1757b52c24
Asim Shrestha <asims178@gmail.com>
2019-05-12 11:27:24 -0400

    Moving the MainMenu Script to the "main" Control

--- gui/mainMenu.gd
+++ gui/mainMenu.gd
@@ -1,15 +1,15 @@
 extends Control
 
 func _ready():
-	var startBtn = $main/VBoxContainer/mainMenuButtons/startGameBtn
-	var settingsBtn = $main/VBoxContainer/mainMenuButtons/settingsBtn
-	var exitBtn = $main/VBoxContainer/mainMenuButtons/exitGameBtn
+	var startBtn = $VBoxContainer/mainMenuButtons/startGameBtn
+	var settingsBtn = $VBoxContainer/mainMenuButtons/settingsBtn
+	var exitBtn = $VBoxContainer/mainMenuButtons/exitGameBtn
 	startBtn.connect("pressed", self, "_set_start_map")
 	settingsBtn.connect("pressed", self, "_show_settings")
 	exitBtn.connect("pressed", self, "_exit_game")
 
 func _set_start_map():
-	visible = false
+	get_parent().visible = false
 	get_node("/root/globals").setMap("house")
 	
 	var inGameUI = get_node("/root/globals").gui.get_node("inGameUI")
@@ -17,8 +17,8 @@ func _set_start_map():
 		inGameUI.visible = true
 
 func _show_settings():
-	get_node("main").visible = false
-	get_node("settings").visible = true
+	visible = false
+	get_parent().get_node("settings").visible = true
 
 func _exit_game():
 	get_tree().quit()
